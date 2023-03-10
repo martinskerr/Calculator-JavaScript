251b845c6aeb6ddffad1d6bc57b154bc4fd8c780
Martin Lopez <martinxx442@gmail.com>
2023-03-10 14:01:15 -0300

    calculadora

---
+++ index.js
@@ -0,0 +1,25 @@
+const uno = document.getElementById("uno");
+const dos = document.getElementById("dos");
+const tres = document.getElementById("tres");
+const cuatro = document.getElementById("cuatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const siete = document.getElementById("siete");
+const ocho = document.getElementById("ocho");
+const nueve = document.getElementById("nueve");
+const cero = document.getElementById("cero");
+const menos = document.getElementById("menos");
+const suma = document.getElementById("suma");
+const multiplicar = document.getElementById("multiplicar");
+const division = document.getElementById("division");
+const calcular = document.getElementById("calcular");
+
+
+
+
+buttonuno.addEvenListener('click',() => {
+
+    document.write("Hola")
+
+
+});
